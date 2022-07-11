236891aba1cb91a0e2e950fcda43686b2738e390
Josephine Heidepriem <jheidepriem@gmail.com>
2022-07-11 10:47:41 -0500

    Add changes to ifStatements assignment

--- ifStatements.js
+++ ifStatements.js
@@ -4,7 +4,7 @@
 // (while navigated to the codeChallenges directory):
 // `node ifStatements.js`
 
-// Example: Using the weather variable below, write code that decides 
+// Example: Using the weather variable below, write code that decides
 // what you should take with you based on the following conditions:
   // if it is sunny, print "sunscreen"
   // if it is rainy, print "umbrella"
@@ -18,9 +18,9 @@ if (weather === "sunny") {
 } else if (weather === "rainy") {
   console.log("umbrella");
 } else if (weather === "snowy") {
-  console.log("coat");
+  console.log("Parka");
 } else if (weather === "icy") {
-  console.log("yak traks");
+  console.log("stay home");
 } else {
   console.log("good to go!");
 }
@@ -36,21 +36,25 @@ if (weather === "sunny") {
 
 // Right now, the program will print
 // out both "I have enough money for a gumball" and
-// "I don't have enough money for a gumball". Write a 
+// "I don't have enough money for a gumball". Write a
 // conditional statement that prints only one or the other.
 
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-numQuarters = 0
+var numQuarters = 0
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+if (numQuarters === 0){
+  console.log ("I don't have enough money for a gumball");
+}
+else if (numQuarters >= .50){
+  console.log("I have enough money for a gumball");
+}
 
 
 // #####################
 // Using the variables defined below, write code that will tell you
-// if you have the ingredients to make a pizza. A pizza requires 
+// if you have the ingredients to make a pizza. A pizza requires
 // at least two cups of flour and sauce.
 
 // You should be able to change the variables to achieve the following outconsole.log();
@@ -65,3 +69,14 @@ console.log("I don't have enough money for a gumball");
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour === 1 && hasSauce === true)
+# true ("I cannot make pizza")
+
+if (cupsOfFlour === 5 && hasSauce === false)
+# false ("I cannot make pizza")
+
+if (cupsOfFlour === 2 && hasSauce === true)
+# true ("I can make pizza")
+
+if (cupsOfFlour === 3 && hasSauce === true)
+# true ("I can make pizza")
