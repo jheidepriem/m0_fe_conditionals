52023da1f55f8f2330fd0e91aa3eb1b1fc44dd89
Josephine Heidepriem <jheidepriem@gmail.com>
2022-07-11 12:18:57 -0500

    Added new code challege file, deleted old files

--- codeChallenges/codeChallenges.js
+++ codeChallenges/codeChallenges.js
@@ -1,10 +1,11 @@
 var number = 6;
 var result = number % 4;
 
-var number =("7");
-if (number % == 0){
-  console.log("The number is even")
+var number = ("7");
+if(number % 2 == 0){
+  console.log ("The number is even")
 }
+
 else {
   console.log("The number is odd");
 }
